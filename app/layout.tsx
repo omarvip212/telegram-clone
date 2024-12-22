8f50702e6d6873a02b34d76de1ceb69f8a939a82
omarvip212 <omarscarf15@gmail.com>
2024-12-22 15:43:35 +0100

    Fix CSS configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { Sidebar } from '@/components/Sidebar'
+import { Sidebar } from '../components/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
